2e42f2468d3d0062fee22008d60d881b34225ff9
Chris Siannas <xrsia@otenet.gr>
2019-10-26 00:56:20 +0300

    4 events now instead of 3

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
     # create an instance var in order to pass data to view
-    @events = ['BugSmash','Hackthon','Kata Camp']
+    @events = ['BugSmash','Hackthon','Kata Camp','Rails User Group']
   end
 end
