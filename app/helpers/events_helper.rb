eeb91da943f573bfd5d12982302ebd16f1480647
Chris Siannas <xrsia@otenet.gr>
2019-10-28 17:55:25 +0200

    content_tag(..) for bold free, another way

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,7 +1,8 @@
 module EventsHelper
   def format_price(event)
     if event.free?
-      "<strong>Free!</strong".html_safe
+      # "<strong>Free!</strong".html_safe
+      content_tag(:strong,"Free")
     else
       number_to_currency(event.price)
     end
