783fd10bb2c125e7596c606d8a398547aae8c7e6
Chris Siannas <xrsia@otenet.gr>
2019-10-29 12:41:48 +0200

    add an event route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # we want /events to match the controller - action pair
   # which is events#index
   get "/events" => "events#index"
-  get "/events/:id"=>"events#show"
+  get "/events/:id"=>"events#show",as: "event"
 end
