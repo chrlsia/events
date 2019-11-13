edf937a4cb40edaf6a8e3ff6d1fc2ecfa5e47012
Chris Siannas <xrsia@otenet.gr>
2019-11-13 19:12:02 +0200

    forms editing 2 -update, redirect_to

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,4 +12,14 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
   
+  def update
+    @event= Event.find(params[:id])
+    event_params = params.require(:event).
+      permit(:name,:description,:location,:price,:starts_at)
+    @event.update(event_params)
+    # render 'show'  render show template,not good
+    # redirect_to event_path(@event) very good or better
+    redirect_to @event #rails knows where to redirect cause @event is the current object
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root "events#index"
   get "/events" => "events#index"
   get "/events/:id"=>"events#show",as: "event"
-  get "/events/:id/edit"=>"events#edit",as: "event_edit"
+  get "/events/:id/edit"=>"events#edit",as: "edit_event"
+  patch "events/:id" => "events#update"
 end
