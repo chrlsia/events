6ae667515ea00344af73eee3e4f684334a82d218
Chris Siannas <xrsia@otenet.gr>
2019-11-17 16:08:31 +0200

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # we want /events to match the controller - action pair
   # which is events#index
-  # root "events#index"
+  root "events#index"
   # get "/events" => "events#index"
   # get '/events/new'=>"events#new"
   # get "/events/:id"=>"events#show",as: "event"
